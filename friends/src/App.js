bfa76f04928117e01ab088253bdf19874a6ca9c1
Timothy Shores <timothymshores@gmail.com>
2019-02-27 16:39:00 -0600

    Updated app.js & Friends.js to pass friends as props from app.js to friends.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Friends from './components/Friends'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
   componentDidMount() {
-    // console.log("App.js componentDidMount()");
     axios
       .get('http://localhost:5000/friends')
-      .then(response => console.log(response.data))
+      .then(response => { this.setState({ friends: response.data }); })
       .catch(err => console.log(err));
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <h1>My Friends List</h1>
+        <Friends friends={this.state.friends} />
       </div>
     );
   }
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,12 +1,24 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
 
 function Friends(props) {
+    if (props.friends.length === 0) {
+        return <h3>Loading friends list...</h3>
+    }
+
     return (
-        console.log(props)
-        < div >
-        <h1>Test</h1>
-        </div >
+        <div className="friends-list">
+            {props.friends.map(friend => (
+                <div className="friend-card" key={friend.id}>
+                    <p>Name: {friend.name}</p>
+                    <p>Age: {friend.age}</p>
+                    <p>Email: {friend.email}</p>
+                    <br />
+                </div>
+            ))}
+        </div>
     );
+
 }
 
 export default Friends;
