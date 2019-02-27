4278162c0b2f3c4ee8ef21058ba62fd95032a6ac
Timothy Shores <timothymshores@gmail.com>
2019-02-27 16:00:08 -0600

    Cleaned up create react app file structure. Imported axios into App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    // console.log("App.js componentDidMount()");
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => console.log(response.data))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hello</h1>
       </div>
     );
   }
---
+++ friends/src/components/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Friends(props) {
+    return (
+        console.log(props)
+        < div >
+        <h1>Test</h1>
+        </div >
+    );
+}
+
+export default Friends;
