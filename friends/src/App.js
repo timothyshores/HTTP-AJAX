dfd600c02b76aa920597c4b4e112155baab79895
Timothy Shores <timothymshores@gmail.com>
2019-04-03 16:34:10 -0500

    Console log response from server

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            friends: [],
+        }
+    }
+
+    componentDidMount() {
+        axios
+            .get('http://localhost:5000/friends')
+            .then(response => console.log('Response: ', response))
+            .catch(err => console.log('Error: ', err));
+    }
+
     render() {
         return (
             <div className="App">
